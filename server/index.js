a02e24f3fcc4d089ea78ac9ca98c724682c3db02
tvieiraramos <tvieiraramos27@hotmail.com>
2021-03-31 20:35:23 -0300

    sabado

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+const server = require("http").Server(app);
+
+const PORT = 3560; 
+app.use(express.static("./jogo_web"));
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`)); 
