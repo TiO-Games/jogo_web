631fb30512eaf56daefdff866e6e1a35cc6a4024
tvieiraramos <tvieiraramos27@hotmail.com>
2021-04-03 16:21:08 -0300

    atual

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 
-const PORT = 3560; 
+const PORT = 3560;
 app.use(express.static("./jogo_web"));
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`)); 
