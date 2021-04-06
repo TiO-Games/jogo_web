ea87b22e53a55236146e1ade1da722f54262673c
tvieirramos <tvieiraramos27@hotmail.com>
2021-04-06 20:08:31 -0300

    tiago

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const server = require("http").Server(app);
 
 const PORT = 3560; 
 
-app.use(express.static("./jogo_web"));
+app.use(express.static("./"));
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
